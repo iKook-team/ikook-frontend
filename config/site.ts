72ce1af3a424b498bf3bfd4c2f48a1b4f3f9cea7
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2025-09-13 13:45:22 +0100

    chore: update doc

--- config/site.ts
+++ config/site.ts
@@ -9,8 +9,8 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Explore Services",
+      href: "/explore",
     },
     {
       label: "Pricing",
