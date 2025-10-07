a9e6e4396d3ede9e219c37e8e9b18cabb64b75a6
Augustus Ezenwankwo <onyeka.ezenwankwo@gmail.com>
2025-10-07 03:37:38 +0100

    refactor: Serve social icons locally

--- components/footer/SocialIcons.tsx
+++ components/footer/SocialIcons.tsx
@@ -9,31 +9,31 @@ interface SocialIconsProps {
 export const SocialIcons: React.FC<SocialIconsProps> = ({ className = "" }) => {
   const socialIcons = [
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/ff501a58d59a405f99206348782d743c/77dea1fdcbc96694e3ad749f15af5312fe8e0cbc?placeholderIfAbsent=true",
+      src: "/socialicons/facebook.svg",
       alt: "Facebook",
       className: "aspect-[1] object-contain w-[30px] shrink-0",
       url: "https://www.facebook.com/share/1Fac1pryvt/?mibextid=wwXIfr",
     },
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/ff501a58d59a405f99206348782d743c/4b46f7a7cf8d0259e3693a646e1e3464e666367b?placeholderIfAbsent=true",
+      src: "/socialicons/twitter.svg",
       alt: "X (Twitter)",
       className: "aspect-[1] object-contain w-[30px] shrink-0",
       url: "https://x.com/ikookapp?s=21",
     },
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/ff501a58d59a405f99206348782d743c/b64b0f4814b721159e3cbd43791319d79f5c3138?placeholderIfAbsent=true",
+      src: "/socialicons/instagram.svg",
       alt: "Instagram",
       className: "aspect-[0.87] object-contain w-[26px] shrink-0",
       url: "https://www.instagram.com/ikookapp?igsh=bzdrZ2diZXZyb2Zi",
     },
     {
-      src: "https://cdn.builder.io/api/v1/image/assets/ff501a58d59a405f99206348782d743c/f1918811a320630d8dc671f2581218dcd98f80df?placeholderIfAbsent=true",
+      src: "/socialicons/linkedin.svg",
       alt: "LinkedIn",
       className: "aspect-[1] object-contain w-[30px] shrink-0",
       url: "https://www.linkedin.com/company/ikookapp/",
     },
     {
-      src: "https://cdn.simpleicons.org/whatsapp/000000",
+      src: "/socialicons/whatsapp.svg",
       alt: "WhatsApp",
       className: "aspect-[1] object-contain w-[30px] shrink-0",
       url: "https://wa.me/2349160006924",
